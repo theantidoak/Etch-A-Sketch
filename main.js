622dcdee4ecd313f208540889b30e4171af4ec0e
theantidoak <tcdoak@gmail.com>
2022-04-07 16:30:54 -0400

    Continue formatting and add chameleon feature on body background

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const container = document.querySelector('.container');
 const interface = document.querySelector(".interface");
 const numberChange = document.querySelector(".change-number");
 const clearButton = document.querySelector(".clear-button");
@@ -6,18 +7,18 @@ const colorPalette = document.querySelector(".color-palette");
 const colorChange = document.querySelector(".change-color");
 const colorSchemes = colorChange.children;
 const colorOption = document.querySelectorAll("option");
-const chameleonButton = document.querySelector('.chameleon-button');
+const chameleonButton = document.querySelector(".chameleon-button");
 const friendlyFireButton = document.querySelector(".friendly-fire");
 const ffToggle = document.querySelector(".ff-toggle-on");
-const shape = document.querySelectorAll('.shape');
-const instructions = document.querySelector('.instructions');
+const shape = document.querySelectorAll(".shape");
+const instructions = document.querySelector(".instructions");
 let flag = false;
 let color = 0;
 let grey = 255;
 let friendlyFire = false;
 let chameleon = false;
 let toggleShape = false;
-let currentScheme = 'default';
+let currentScheme = "default";
 
 function createInterface() {
   let number = Math.floor(numberChange.value);
@@ -51,44 +52,82 @@ function colorEachSquare() {
 
   if (flag) {
     switch (currentScheme) {
-      case 'default':
+      case "default":
         if (friendlyFire) {
-          white == true ? this.style.backgroundColor = 'black' : null;
+          white == true ? (this.style.backgroundColor = "black") : null;
         } else {
           this.style.backgroundColor = "black";
         }
         break;
-      case 'color-wheel':
+      case "color-wheel":
         if (friendlyFire) {
-          white == true ? this.style.backgroundColor = `${colorPalette.value}` : null;
+          white == true
+            ? (this.style.backgroundColor = `${colorPalette.value}`)
+            : null;
         } else {
           this.style.backgroundColor = `${colorPalette.value}`;
         }
         break;
-      case 'random':
-        chameleon ? [...interface.children].forEach((e) => e.style.backgroundColor = `rgb(${Math.floor(Math.random() * 245)}, ${Math.floor(Math.random() * 245)}, ${Math.floor(Math.random() * 245)})`) : null;
+      case "random":
+        chameleon
+          ? [...interface.children].forEach(
+              (e) =>
+                (e.style.backgroundColor = `rgb(${Math.floor(
+                  Math.random() * 245
+                )}, ${Math.floor(Math.random() * 245)}, ${Math.floor(
+                  Math.random() * 245
+                )})`)
+            )
+          : null;
         if (friendlyFire) {
-          white == true ? this.style.backgroundColor = `rgb(${Math.floor(Math.random() * 245)}, ${Math.floor(Math.random() * 245)}, ${Math.floor(Math.random() * 245)})` : null;
+          white == true
+            ? (this.style.backgroundColor = `rgb(${Math.floor(
+                Math.random() * 245
+              )}, ${Math.floor(Math.random() * 245)}, ${Math.floor(
+                Math.random() * 245
+              )})`)
+            : document.body.style.backgroundColor = `rgb(${Math.floor(
+              Math.random() * 245
+            )}, ${Math.floor(Math.random() * 245)}, ${Math.floor(
+              Math.random() * 245
+            )})`;
         } else {
-          this.style.backgroundColor = `rgb(${Math.floor(Math.random() * 245)}, ${Math.floor(Math.random() * 245)}, ${Math.floor(Math.random() * 245)})`;
+          this.style.backgroundColor = `rgb(${Math.floor(
+            Math.random() * 245
+          )}, ${Math.floor(Math.random() * 245)}, ${Math.floor(
+            Math.random() * 245
+          )})`;
         }
         break;
-      case 'rainbow':
-        chameleon ? [...interface.children].forEach((e) => (e.style.backgroundColor = `hsl(${Math.floor(Math.random() * 356)}, 100%, 50%)`)) : null;
+      case "rainbow":
+        chameleon
+          ? [...interface.children].forEach(
+              (e) =>
+                (e.style.backgroundColor = `hsl(${Math.floor(
+                  Math.random() * 356
+                )}, 100%, 50%)`)
+            )
+          : null;
         if (friendlyFire) {
-          white == true ? this.style.backgroundColor = `hsl(${color * 2}, 100%, 50%)` : null;
+          white == true
+            ? (this.style.backgroundColor = `hsl(${color * 2}, 100%, 50%)`)
+            : document.body.style.backgroundColor = `hsl(${color * 2}, 100%, 50%)`;
         } else {
           this.style.backgroundColor = `hsl(${color * 2}, 100%, 50%)`;
         }
         color++;
         break;
-      case 'shades-of-grey':
-        chameleon ? [...interface.children].forEach((e) => {
-          grey = Math.floor(Math.random() * 255);
-          (e.style.backgroundColor = `rgb(${grey}, ${grey}, ${grey})`)
-        }) : null;
+      case "shades-of-grey":
+        chameleon
+          ? [...interface.children].forEach((e) => {
+              grey = Math.floor(Math.random() * 255);
+              e.style.backgroundColor = `rgb(${grey}, ${grey}, ${grey})`;
+            })
+          : null;
         if (friendlyFire) {
-          black == true ? this.style.backgroundColor = `rgb(${grey}, ${grey}, ${grey})` : null;
+          black == true && friendlyFire == false
+            ? (this.style.backgroundColor = `rgb(${grey}, ${grey}, ${grey})`)
+            : document.body.style.backgroundColor =`rgb(${grey}, ${grey}, ${grey})`;
         } else {
           this.style.backgroundColor = `rgb(${grey}, ${grey}, ${grey})`;
         }
@@ -102,17 +141,21 @@ function colorEachSquare() {
 }
 
 function clearInterface() {
+  container.classList.add('shake');
   if (interface.firstElementChild == instructions) {
     interface.removeChild(instructions);
     createInterface();
   }
   if (currentScheme == "shades-of-grey") {
-    [...interface.children].forEach((e) => e.style.backgroundColor = "black");
-    interface.style.backgroundColor = 'black';
+    [...interface.children].forEach((e) => (e.style.backgroundColor = "black"));
+    interface.style.backgroundColor = "black";
     grey = 255;
   } else {
-    [...interface.children].forEach((e) => e.style.backgroundColor = "white");
+    [...interface.children].forEach((e) => (e.style.backgroundColor = "white"));
   }
+  setTimeout(() => container.classList.remove('shake'), 80);
+  setTimeout(() => container.classList.add('shake'), 160);
+  setTimeout(() => container.classList.remove('shake'), 240);
 }
 
 function changeColorScheme() {
@@ -120,15 +163,30 @@ function changeColorScheme() {
     interface.removeChild(instructions);
     createInterface();
   }
-  [...colorSchemes].forEach((scheme) => scheme.style.backgroundColor = 'gold' ? scheme.style.backgroundColor = 'white'  : null);
+  [...colorSchemes].forEach(
+    (scheme) => {
+      if (scheme.style.backgroundColor = "white") {
+        scheme.style.backgroundColor = "#003366";
+        scheme.style.color = 'white';
+      }
+    });
   const interfaceSquares = document.querySelectorAll(".interface div");
-  [...colorSchemes].forEach((scheme) => scheme.value == this.value ? scheme.style.backgroundColor = 'gold' : null);
+  [...colorSchemes].forEach((scheme) => {
+    if (scheme.value == this.value) {
+      scheme.style.backgroundColor = 'white';
+      scheme.style.color = '#003366';
+    }
+  });
   if (this.value == "shades-of-grey") {
-    interfaceSquares.forEach((square) => square.style.backgroundColor = "black");
-    interface.style.backgroundColor = 'black';
+    interfaceSquares.forEach(
+      (square) => (square.style.backgroundColor = "black")
+    );
+    interface.style.backgroundColor = "black";
   } else {
-    interface.style.backgroundColor = 'white';
-    interfaceSquares.forEach((square) => square.style.backgroundColor = "white");
+    interface.style.backgroundColor = "white";
+    interfaceSquares.forEach(
+      (square) => (square.style.backgroundColor = "white")
+    );
   }
   if (this.value == "color-wheel") {
     colorPalette.style.visibility = "visible";
@@ -146,28 +204,28 @@ function changeColorScheme() {
 function toggleFriendlyFire() {
   if (friendlyFire) {
     friendlyFireButton.textContent = "🔥";
-    friendlyFireButton.style.backgroundColor = 'black';
+    friendlyFireButton.style.backgroundColor = "white";
   } else {
     friendlyFireButton.textContent = "🔥";
-    friendlyFireButton.style.backgroundColor = 'white';
+    friendlyFireButton.style.backgroundColor = "#003366";
   }
   friendlyFire = !friendlyFire;
-};
+}
 
 function toggleChameleon() {
   if (!chameleon) {
-    chameleonButton.textContent = '🦎';
-    chameleonButton.style.backgroundColor = 'black';
+    chameleonButton.textContent = "🦎";
+    chameleonButton.style.backgroundColor = "white";
   } else {
-    chameleonButton.textContent = '🦎';
-    chameleonButton.style.backgroundColor = 'white';
+    chameleonButton.textContent = "🦎";
+    chameleonButton.style.backgroundColor = "#003366";
   }
   if (currentScheme == "shades-of-grey") {
-    [...interface.children].forEach((e) => e.style.backgroundColor = "black");
-    interface.style.backgroundColor = 'black';
+    [...interface.children].forEach((e) => (e.style.backgroundColor = "black"));
+    interface.style.backgroundColor = "black";
     grey = 255;
   } else {
-    [...interface.children].forEach((e) => e.style.backgroundColor = "white");
+    [...interface.children].forEach((e) => (e.style.backgroundColor = "white"));
   }
   chameleon = !chameleon;
 }
@@ -175,18 +233,17 @@ function toggleChameleon() {
 function toggleDivShape() {
   const interfaceSquares = document.querySelectorAll(".interface div");
   if (toggleShape) {
-    interfaceSquares.forEach((square) => square.style.borderRadius = '50%');
+    interfaceSquares.forEach((square) => (square.style.borderRadius = "50%"));
     shape.forEach((shapes) => {
-      shapes.textContent = 'Change to Square';
-      shapes.style.borderRadius = '50%';
+      shapes.style.borderRadius = "50%";
     });
   } else {
-    interfaceSquares.forEach((square) => square.style.borderRadius = '0');
+    interfaceSquares.forEach((square) => (square.style.borderRadius = "0"));
     shape.forEach((shapes) => {
-      shapes.textContent = 'Change to Circle';
-      shapes.style.borderRadius = '0';
-      shapes.style.height = '100px';
-      shapes.style.width = '100px';
+      shapes.style.borderRadius = "0";
+      shapes.style.height = "86px";
+      shapes.style.width = "86px";
+      shapes.textContent = '';
     });
   }
   toggleShape = !toggleShape;
@@ -195,8 +252,9 @@ function toggleDivShape() {
 // createInterface();
 numberChange.addEventListener("input", recalculateSquares);
 clearButton.addEventListener("click", clearInterface);
-[...colorSchemes].forEach((scheme) => scheme.addEventListener("click", changeColorScheme));
+[...colorSchemes].forEach((scheme) =>
+  scheme.addEventListener("click", changeColorScheme)
+);
 friendlyFireButton.addEventListener("click", toggleFriendlyFire);
 chameleonButton.addEventListener("click", toggleChameleon);
-shape.forEach((shapes) => shapes.addEventListener('click', toggleDivShape));
-
+shape.forEach((shapes) => shapes.addEventListener("click", toggleDivShape));
